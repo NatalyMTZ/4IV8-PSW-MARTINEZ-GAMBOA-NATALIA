2b7a17b925d889dc4f9cc05717b5c8b08eab0ae8
NatalyMTZ <nmartinezg1902@alumno.ipn.mx>
2021-05-02 15:52:13 -0500

    Examen

---
+++ Clase19/HolaMundo.java
@@ -0,0 +1,21 @@
+class HolaMundo{
+    //aqui es donde va estar todo nuestro codigo de programacion
+    //esto es el cuerpo del programa
+
+    //clase principal
+
+    public static void main(String[] args){
+        /*
+        este es el metodo principal de cualquier clase de java
+        en el cual se especifica que va a poder recibir cadenas como
+        argumentos 
+        los argumentos son cualquier tipo de valor que puede recibir 
+        este metodo principal, aqui es donde nosotro especificamos todo
+        el cuerpo del algoritmo
+        */ 
+
+        System.out.print("Hola Mundo, ya no eres kawaii");
+        //Esto es lo mismo que:
+        //Escribir "Hola Mundo ¬¬, ya no eres kawaii"
+    } 
+}
